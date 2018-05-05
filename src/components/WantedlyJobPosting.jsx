5b83fbaa0ae7f5d9a0d9c240698c37eaf0f5f0cd
mrdShinse <shinse1128@hotmail.com>
2018-05-05 23:43:06 +0900

    Refactor/ WantedlyJobPosting.

--- src/components/WantedlyJobPosting.jsx
+++ src/components/WantedlyJobPosting.jsx
@@ -1,169 +1,178 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const styles = {
-  container: {
-    paddingTop: 10,
-    paddingBottom: 8,
-    borderBottomColor: '#e3e3e3',
-    borderBottomStyle: 'solid',
-    borderBottomWidth: 1,
-  },
-  innerContainer: {
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  coverImageContainer: {
-    marginBottom: 20,
-  },
-  coverImage: {
-    width: 560,
-    height: 228,
-    overflow: 'hidden',
-    backgroundColor: '#1AA4BA', // temporary
-  },
-  jobTypeLabelContainer: {
-    display: 'flex'
-  },
-  jobTypeLabelContainer: {
-    fontFamily: '"Helvetica Neue", Helvetica, Arial, "Hiragino Sans", "ヒラギノ角ゴシック", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ Pro W3",Roboto, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", sans-serif',
-    borderRadius: 2,
-    display: 'block',
-    boxSizing: 'border-box',
-    margiRight: 8,
-    paddingLeft: 4,
-    paddingRight: 4,
-    height: 20,
-    color: '#1AA4BA',
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#1AA4BA',
-    fontSize: 11,
-    fontWeight: 500,
-    lineHeight: '18px',
-  },
-  jobTitle: {
-    fontSize: 22,
-    fontWeight: 600,
-    lineHeight: 1.4,
-    marginTop: 10,
-    marginBottom: 8,
-  },
-  jobDescription: {
-    lineHeight: 1.5,
-    color: '#9DA0A4',
-    marginBottom: 14,
-    fontSize: 14,
-    fontWeight: 400,
-    wordWrap: 'break-word',
-  },
-  footer: {
-    display: 'flex',
-    marginTop: 16,
-  },
-  companyImageContainer: {
-    width: 50,
-    height: 50,
-    paddingRight: 12,
-  },
-  companyImage: {
-    height: '100%',
-    backgroundColor: '#000000',
-    borderRadius: '50%',
-  },
-  companyNameContainer: {
-    flexShrink: 1,
-    flexBasis: 'auto',
-    minWidth: 0,
-  },
-  companyName: {
-    fontSize: 14,
-    fontWeight: 500,
-    lineHeight: '1.3em',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  },
-  actionsContainer: {
-    flexGrow: 1,
-    flexBasis: 'auto',
-    textAlign: 'right',
-  },
-  publishedAt: {
-    display: 'inline-block',
-    paddingRight: 15,
-    marginRight: 15,
-    marginLeft: 15,
-    borderRightWidth: 1,
-    borderRightStyle: 'solid',
-    borderRightColor: '#ccc',
-    fontSize: 11,
-    lineHeight: '20px',
-    color: '#ccc',
-  },
-  supportPointsContainer: {
-    fontSize: 18,
-    color: '#aaa',
-    display: 'inline-block',
-    verticalAlign: 'middle',
-  },
-  supportCount: {
-    fontWeight: 600,
-  },
-  bookmarkButtonContainer: {
-    display: 'inline-block',
-    paddingLeft: 10,
-    paddingRight: 10,
+import noimage from '../images/noimage.svg';
+import logo from '../images/logo.svg';
+
+const Wrapper = styled.div`
+  padding: 30px 0px 28px 0px;
+  border-bottom: solid 1px #e3e3e3;
+  width: 560px;
+
+  .cover {
+    margin: 0 0 20px;
+    height: 228px;
+    overflow: hidden;
   }
-};
+
+  .jobtype {
+    display: flex;
+
+    span {
+      height: 20px;
+      line-height: 18px;
+      font-family: "Helvetica Neue", Helvetica, Arial, "Hiragino Sans", "ヒラギノ角ゴシック", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ Pro W3",Roboto, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", sans-serif;
+      font-size: 11px;
+      font-weight: 500;
+      display: block;
+      box-sizing: border-box;
+      margin: 0 8px 0 0;
+      padding: 0 4px 0 4px;
+      color: #1AA4BA;
+      background-color: transparent;
+      border: solid 1px #1AA4BA;
+      border-radius: 2px;
+    }
+  }
+
+  .title {
+    font-size: 22px;
+    font-weight: 600;
+    line-height: 1.4em;
+    margin: 10px 0 8px 0;
+  }
+
+  .description {
+    line-height: 1.5em;
+    color: #9DA0A4;
+    margin: 0 0 14px;
+    font-size: 14px;
+    font-weight: 400;
+    word-wrap: break-word;
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    margin: 16px 0 0 0;
+    width: 100%;
+
+    .companylogo {
+      width: 50px;
+      height: 50px;
+      padding: 0 12px 0 0;
+
+      img {
+        height: 100%;
+        border-radius: 50%;
+        background-color: #000000;
+      }
+    }
+
+    .companyname {
+      flex: 0 1 auto;
+      min-width: 0;
+
+      h3 {
+        margin: 0;
+        font-size: 14px;
+        font-weight: 500;
+        color: #24282a;
+        line-height: 1.3em;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
+
+    .actions {
+      flex: 1 0 auto,
+      text-align: right;
+
+      .published-date {
+        display: inline-block;
+        padding: 0 15px 0 0;
+        margin: 0 15px;
+        border-right: 1px solid #ccc;
+        font-size: 11px;
+        line-height: 20px;
+        color: #ccc;
+      }
+
+      .support-points {
+        font-size: 18px;
+        color: #aaa;
+        display: inline-block;
+        vertical-align: middle;
+
+        a {
+          font-weight: 600;
+        }
+      }
+
+      .bookmark-form {
+        display: inline-block;
+        vartical-align: middle;
+        line-height: 20px;
+        padding: 0 10px 0 10px;
+        margin: 0px;
+
+        .bookmark-button {
+          color: #aaa;
+        }
+      }
+    }
+  }
+`
 
 const WantedlyJobPosting = () => {
   return (
-    <div style={styles.container}>
-      <div style={styles.innerContainer}>
-        <div>
-          <div style={styles.coverImageContainer}>
-            <div style={styles.coverImage}>
-              <img />
-            </div>
-          </div>
-          <div style={styles.jobTypeLabelContainer}>
-            <span style={styles.jobTypeLabel}>Web engineer</span>
-          </div>
-          <h1 style={styles.jobTitle}>
-            <a>フロントエンドエンジニア募集！</a>
-          </h1>
-          <p style={styles.jobDescription}>
-            あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。 またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガン...
-          </p>
+    <Wrapper>
+      <div>
+        <div className="cover">
+          <img src={noimage} alt='cover-image' />
         </div>
-        <div style={styles.footer}>
-          <div style={styles.companyImageContainer}>
-            <div style={styles.companyImage}>
-              image
-            </div>
+        <div className="jobtype">
+          <span>Web engineer</span>
+        </div>
+        <h1 className='title'>
+          <a>フロントエンドエンジニア募集！</a>
+        </h1>
+        <p className="description">
+          あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。 またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガン...
+        </p>
+      </div>
+
+      <div className="footer">
+        <div className="companylogo">
+          <img src={logo} alt='logo' />
+        </div>
+
+        <div className="companyname">
+          <h3>
+            <a>株式会社カブシキガイシャ</a>
+          </h3>
+        </div>
+
+        <div className="actions">
+          <div className="published-date">
+            <i class="far fa-clock"/>
+            1 day ago
           </div>
-          <div style={styles.companyNameContainer}>
-            <h3 style={styles.companyName}>
-              株式会社カブシキガイシャ
-            </h3>
+          <div className="support-points">
+            <a>
+              <i class="fas fa-volume-down"/>
+                23
+            </a>
           </div>
-          <div style={styles.actionsContainer}>
-            <div style={styles.publishedAt}>1 day ago</div>
-            <div style={styles.supportPointsContainer}>
-              <a>
-                <span style={styles.supportCount}>
-                  23
-                </span>
-              </a>
-            </div>
-            <div style={styles.bookmarkButtonContainer}>
-              b
+          <div className="bookmark-form">
+            <div className="bookmark-button">
+              <i class="far fa-bookmark"/>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Wrapper>
   )
 };
 
